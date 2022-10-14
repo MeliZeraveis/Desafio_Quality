68c1127c67f4ef78565e8411d5882445b17c601b
Bruna Campos <bruna.camposf@mercadolivre.com>
2022-10-14 19:13:09 -0300

    getProperty By Id

--- src/test/java/br/com/dh/desafio_quality/controller/PropertyControllerTest.java
+++ src/test/java/br/com/dh/desafio_quality/controller/PropertyControllerTest.java
@@ -3,6 +3,7 @@ package br.com.dh.desafio_quality.controller;
 import br.com.dh.desafio_quality.dto.DistrictDTO;
 import br.com.dh.desafio_quality.dto.PropertyRequestDTO;
 import br.com.dh.desafio_quality.dto.PropertyResponseDTO;
+import br.com.dh.desafio_quality.enums.Msg;
 import br.com.dh.desafio_quality.exception.NotFoundException;
 import br.com.dh.desafio_quality.model.District;
 import br.com.dh.desafio_quality.model.Property;
@@ -35,7 +36,9 @@ import java.util.List;
 import java.util.UUID;
 
 import static org.mockito.Mockito.atLeastOnce;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
@@ -72,35 +75,61 @@ class PropertyControllerTest {
 
     }
 
-//    @Test
-//    @DisplayName("Tests the method that gets a property by ID - Controller")
-//    void getPropertyById_WhenIdIsValid_ThenReturnProperty() throws NotFoundException {
-//
-//    }
+    @Test
+    @DisplayName("Tests the method that gets a property by ID - Controller")
+    void getPropertyById_WhenIdIsValid_ThenReturnProperty() throws NotFoundException, Exception {
+        BDDMockito.given(service.getProperty(ArgumentMatchers.any(UUID.class))).willReturn(propertyResponse);
+
+        ResultActions response = mockMvc.perform(
+                get("/property/{id}", UUID.randomUUID())
+                        .contentType(MediaType.APPLICATION_JSON));
+
+
+        response.andExpect(status().isOk())
+                .andExpect(jsonPath("$.propName", CoreMatchers.is(propertyResponse.getPropName())))
+                .andExpect(jsonPath("$.propDistrict", CoreMatchers.is(propertyResponse.getPropDistrict())));
+    }
 
     @Test
-    @DisplayName("Tests the method that posts a property - Controller")
-    void postProperty_WhenPropertyIsValid_ThenReturnProperty() throws Exception {
+    @DisplayName("Tests the method that gets a property by ID - Controller")
+    void getPropertyById_WhenIdIsValid_ThenReturnException() throws NotFoundException, Exception {
+        BDDMockito.given(service.getProperty(ArgumentMatchers.any())).willThrow(new NotFoundException(Msg.PROPERTY_NOT_FOUND));
 
-        List<Room> listRooms = new ArrayList<>();
-        listRooms.add(new Room("Sala de estar", 4, 5 ));
-        listRooms.add(new Room("Cozinha", 10, 10 ));
-        DistrictDTO district = new DistrictDTO("Copacabana", BigDecimal.valueOf(1000) );
+        ResultActions response = mockMvc.perform(
+                get("/property/{id}", propertyResponse.getId())
+                        .contentType(MediaType.APPLICATION_JSON));
 
-        PropertyRequestDTO propertyRequest = new PropertyRequestDTO("Casa", district, listRooms);
 
-        BDDMockito.when(service.postProperty(ArgumentMatchers.any())).thenReturn(propertyResponse);
-
-        ResultActions resposta = mockMvc.perform(
-                post("/property")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(propertyRequest)));
-
-        resposta.andExpect(status().isCreated());
-//        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
-//        assertThat(response.getBody()).isNull();
-//        assertThat(response.getBody().getId()).isNotNull();
-//        verify(service, atLeastOnce()).postProperty(property);
-
+        response.andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.title", CoreMatchers.is(Msg.PROPERTY_NOT_FOUND)))
+                .andExpect(jsonPath("$.message", CoreMatchers.is(Msg.PROPERTY_NOT_FOUND)))
+                .andExpect(jsonPath("$.status", CoreMatchers.is(HttpStatus.NOT_FOUND.value())));
     }
+
+
+//    @Test
+//    @DisplayName("Tests the method that posts a property - Controller")
+//    void postProperty_WhenPropertyIsValid_ThenReturnProperty() throws Exception {
+//
+//        List<Room> listRooms = new ArrayList<>();
+//        listRooms.add(new Room("Sala de estar", 4, 5 ));
+//        listRooms.add(new Room("Cozinha", 10, 10 ));
+//        DistrictDTO district = new DistrictDTO("Copacabana", BigDecimal.valueOf(1000) );
+//
+//        PropertyRequestDTO propertyRequest = new PropertyRequestDTO("Casa", district, listRooms);
+//
+//        BDDMockito.when(service.postProperty(ArgumentMatchers.any())).thenReturn(propertyResponse);
+//
+//        ResultActions resposta = mockMvc.perform(
+//                post("/property")
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .content(objectMapper.writeValueAsString(propertyRequest)));
+//
+//        resposta.andExpect(status().isCreated());
+////        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+////        assertThat(response.getBody()).isNull();
+////        assertThat(response.getBody().getId()).isNotNull();
+////        verify(service, atLeastOnce()).postProperty(property);
+//
+//    }
 }
