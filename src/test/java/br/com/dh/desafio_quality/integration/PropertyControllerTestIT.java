5a6903d3c588d81859659e3fce699302a43ad2d1
HugoDaniel2000MELI <hugo.caxias@mercadolivre.com>
2022-10-14 16:05:01 -0300

    test: implementing test complete

--- src/test/java/br/com/dh/desafio_quality/integration/PropertyControllerTestIT.java
+++ src/test/java/br/com/dh/desafio_quality/integration/PropertyControllerTestIT.java
@@ -2,6 +2,8 @@ package br.com.dh.desafio_quality.integration;
 
 import br.com.dh.desafio_quality.dto.DistrictDTO;
 import br.com.dh.desafio_quality.dto.PropertyRequestDTO;
+import br.com.dh.desafio_quality.enums.Msg;
+import br.com.dh.desafio_quality.exception.ExceptionDetails;
 import br.com.dh.desafio_quality.model.Property;
 import br.com.dh.desafio_quality.model.Room;
 import br.com.dh.desafio_quality.repository.PropertyRepo;
@@ -13,11 +15,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 //import java.util.UUID;
@@ -40,8 +44,6 @@ public class PropertyControllerTestIT {
     @Autowired
     private ObjectMapper objectMapper;
 
-    private PropertyRequestDTO propertyRequest;
-
     @BeforeEach
     void setup() {
         repository.deleteAll();
@@ -50,8 +52,8 @@ public class PropertyControllerTestIT {
         listRooms.add(new Room("quarto", 4, 5 ));
         listRooms.add(new Room("cozinha", 10, 10 ));
         DistrictDTO district = new DistrictDTO("Tijuca", BigDecimal.valueOf(1000) );
-        propertyRequest = new PropertyRequestDTO("Casa", district, listRooms);
-        repository.save(new Property(propertyRequest.getPropName(), propertyRequest.getPropDistrict(), propertyRequest.getRooms()));
+        PropertyRequestDTO propertyRequestBefore = new PropertyRequestDTO("Casa", district, listRooms);
+        repository.save(new Property(propertyRequestBefore.getPropName(), propertyRequestBefore.getPropDistrict(), propertyRequestBefore.getRooms()));
     }
 
 
@@ -63,25 +65,30 @@ public class PropertyControllerTestIT {
 
         DistrictDTO district = new DistrictDTO("Copacabana", BigDecimal.valueOf(1000) );
 
-        propertyRequest = new PropertyRequestDTO("Casa", district, listRooms);
+        PropertyRequestDTO propertyRequest = new PropertyRequestDTO("Casa", district, listRooms);
 
-        ResultActions response = mockMvc.perform(post("/property")
+        ResultActions response = mockMvc
+                .perform(post("/property")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(propertyRequest)));
 
         response.andExpect(status().isCreated())
                 .andExpect(jsonPath("$.propName", CoreMatchers.is(propertyRequest.getPropName())))
+                .andExpect(jsonPath("$.propDistrict", CoreMatchers
+                        .is(propertyRequest.getPropDistrict().getName())))
                 .andExpect(jsonPath("$.propArea", CoreMatchers.is(120.0)))
                 .andExpect(jsonPath("$.propValue", CoreMatchers.is(120000.0)));
 
         assertThat(repository.getAll().size()).isEqualTo(2);
+        assertThat(repository.getAll().get(0)).isNotNull();
     }
 
     @Test
     void getProperty_returnProperty_Success() throws Exception {
         Property property = repository.getAll().get(0);
 
-        ResultActions response = mockMvc.perform(get("/property/{id}", property.getId() )
+        ResultActions response = mockMvc
+                .perform(get("/property/{id}", property.getId() )
                 .contentType(MediaType.APPLICATION_JSON));
 
         response.andExpect(status().isOk())
@@ -94,18 +101,13 @@ public class PropertyControllerTestIT {
 
     @Test
     void getProperty_returnProperty_NotFound() throws Exception {
-        Property property = repository.getAll().get(0);
-
-        ResultActions response = mockMvc.perform(get("/property/{id}", "3275b207-4711-4801-9e29-1bd09ae71faf" )
+        ResultActions response = mockMvc
+                .perform(get("/property/{id}", "3275b207-4711-4801-9e29-1bd09ae71faf" )
                 .contentType(MediaType.APPLICATION_JSON));
 
-        response.andExpect(status().isOk())
-                .andExpect(jsonPath("$.id", CoreMatchers.is("3275b207-4711-4801-9e29-1bd09ae71faf")))
-                .andExpect(jsonPath("$.propName", CoreMatchers.is(property.getPropName())))
-                .andExpect(jsonPath("$.propArea", CoreMatchers.is(120.0)))
-                .andExpect(jsonPath("$.propValue", CoreMatchers.is(120000.0)));
-
+        response.andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.title", CoreMatchers.is(Msg.OBJECT_NOT_FOUND)))
+                .andExpect(jsonPath("$.message", CoreMatchers.is(Msg.PROPERTY_NOT_FOUND)))
+                .andExpect(jsonPath("$.status", CoreMatchers.is(HttpStatus.NOT_FOUND.value())));
     }
-
-
 }
