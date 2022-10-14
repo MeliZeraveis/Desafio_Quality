80fbf27c347f63da0308a82724aa9f95750d4566
Rosy Mariana Nunes dos Santos <rosy.santos@mercadolivre.com>
2022-10-14 14:10:43 -0300

    exceptions

--- src/main/java/br/com/dh/desafio_quality/dto/RoomDTO.java
+++ src/main/java/br/com/dh/desafio_quality/dto/RoomDTO.java
@@ -1,14 +1,20 @@
 package br.com.dh.desafio_quality.dto;
 
+import br.com.dh.desafio_quality.enums.Msg;
 import br.com.dh.desafio_quality.model.Room;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @NoArgsConstructor
 public class RoomDTO {
+  @NotBlank(message = Msg.NAME_NOT_EMPTY)
+  @Size(min = 1, max = 30, message = Msg.NAME_SIZE_NOT_VALID)
   private String roomName;
   private double roomArea;
 
---
+++ src/main/java/exception/ExceptionDetails.java
@@ -0,0 +1,16 @@
+package exception;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class ExceptionDetails {
+    private String title;
+    private int status;
+    private String message;
+    private LocalDateTime timeStamp;
+
+}
