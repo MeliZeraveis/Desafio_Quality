8031d596de3dee3a0154075ab7c4fc934213337f
Bruna Campos <bruna.camposf@mercadolivre.com>
2022-10-17 14:34:33 -0300

    style: clean unused imports

--- src/test/java/br/com/dh/desafio_quality/controller/PropertyControllerTest.java
+++ src/test/java/br/com/dh/desafio_quality/controller/PropertyControllerTest.java
@@ -5,28 +5,20 @@ import br.com.dh.desafio_quality.dto.PropertyRequestDTO;
 import br.com.dh.desafio_quality.dto.PropertyResponseDTO;
 import br.com.dh.desafio_quality.enums.Msg;
 import br.com.dh.desafio_quality.exception.NotFoundException;
-import br.com.dh.desafio_quality.model.District;
 import br.com.dh.desafio_quality.model.Property;
 import br.com.dh.desafio_quality.model.Room;
-import br.com.dh.desafio_quality.service.IProperty;
 import br.com.dh.desafio_quality.service.PropertyService;
-//import br.com.dh.desafio_quality.util.TestUtilsGenerator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.CoreMatchers;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentMatchers;
 import org.mockito.BDDMockito;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
@@ -36,14 +28,10 @@ import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
-import static org.mockito.Mockito.atLeastOnce;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.verify;
-
 
 @WebMvcTest(PropertyController.class)
 class PropertyControllerTest {
@@ -91,14 +79,13 @@ class PropertyControllerTest {
                 get("/property/{id}", UUID.randomUUID())
                         .contentType(MediaType.APPLICATION_JSON));
 
-
         response.andExpect(status().isOk())
                 .andExpect(jsonPath("$.propName", CoreMatchers.is(propertyResponse.getPropName())))
                 .andExpect(jsonPath("$.propDistrict", CoreMatchers.is(propertyResponse.getPropDistrict())));
     }
 
     @Test
-    @DisplayName("Tests the method that gets a property by ID - Controller")
+    @DisplayName("Tests the method that gets a property by ID and return an Exception - Controller")
     void getPropertyById_WhenIdIsValid_ThenReturnException() throws NotFoundException, Exception {
         BDDMockito.given(service.getProperty(ArgumentMatchers.any())).willThrow(new NotFoundException(Msg.PROPERTY_NOT_FOUND));
 
@@ -108,11 +95,7 @@ class PropertyControllerTest {
 
 
         response.andExpect(status().isNotFound());
-//                .andExpect(jsonPath("$.title", CoreMatchers.is(Msg.PROPERTY_NOT_FOUND)))
-//                .andExpect(jsonPath("$.message", CoreMatchers.is(Msg.PROPERTY_NOT_FOUND)))
-//                .andExpect(jsonPath("$.status", CoreMatchers.is(HttpStatus.NOT_FOUND.value())));
     }
-
 
     @Test
     @DisplayName("Tests the method that posts a property - Controller")
@@ -133,10 +116,5 @@ class PropertyControllerTest {
                         .content(objectMapper.writeValueAsString(propertyRequest)));
 
         resposta.andExpect(status().isCreated());
-//        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
-//        assertThat(response.getBody()).isNull();
-//        assertThat(response.getBody().getId()).isNotNull();
-//        verify(service, atLeastOnce()).postProperty(property);
-
     }
 }
